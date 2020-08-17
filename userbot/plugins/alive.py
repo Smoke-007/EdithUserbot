538b639e2da547f128e333667dd3b8d551e7ec7d
TeamAtul <68645655+TeamAtul@users.noreply.github.com>
2020-08-17 18:42:24 +0530

    Create alive.py

---
+++ userbot/plugins/alive.py
@@ -0,0 +1,28 @@
+"""Check if userbot alive."""
+import asyncio
+from telethon import events
+from uniborg.util import admin_cmd
+from userbot import ALIVE_NAME
+from telethon.tl.types import ChannelParticipantsAdmins
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+PM_IMG = "https://telegra.ph/file/fb10fea3d152e1524ef93.png"
+pm_caption = "`E.D.I.T.H. IS:` **ONLINE**\n\n"
+pm_caption += "**SYSTEM STATUS**\n"
+pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
+pm_caption += "`DATABASE STATUS:` **Functional**\n"
+pm_caption += "**Current Branch** : `piro`\n"
+pm_caption += "**Edith OS** : `3.14`\n"
+pm_caption += "**Current Sat** : `TeamAtulSat-2.25`\n"
+pm_caption += f"**My Master** : {DEFAULTUSER} \n"
+pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption += "**License** : [MIT Licence](github.com/TeamAtul/EdithTheUserbot/blob/master/LICENSE)\n"
+pm_caption += "Copyright : By [TeamAtul@Github](GitHub.com/TeamAtul)\n"
+pm_caption += " [Deploy EdithUserbot](https://telegra.ph/EDITH-The-Userbot-08-12)"
+#@command(outgoing=True, pattern="^.alive$")
+@borg.on(admin_cmd(pattern=r"alive"))
+async def amireallyalive(alive):
+    chat = await alive.get_chat()
+    await alive.delete()
+    """ For .alive command, check if the bot is running.  """
+    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await alive.delete() 
