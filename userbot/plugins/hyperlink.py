3159fe9fcaffb26d2a9bfb7aa383cfbce66d4cab
TeamAtul <68645655+TeamAtul@users.noreply.github.com>
2020-08-17 19:12:42 +0530

    Initial commit

---
+++ userbot/plugins/hyperlink.py
@@ -0,0 +1,11 @@
+from telethon import events
+from userbot.utils import admin_cmd
+import asyncio
+from telethon.tl import functions, types
+
+@borg.on(admin_cmd(pattern="hl ?(.*)"))
+async def _(event):
+        if event.fwd_from:
+            return
+        input = event.pattern_match.group(1)
+        await event.edit("[ㅤㅤㅤㅤㅤㅤㅤ](" + input + ")")
