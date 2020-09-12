0fa74188b54e0b484ac5ec6a05e3510daee55187
TeamAtul <68645655+TeamAtul@users.noreply.github.com>
2020-09-12 20:37:37 +0530

    Update mode_on.py

--- userbot/plugins/mode_on.py
+++ userbot/plugins/mode_on.py
@@ -7,10 +7,10 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/e5c05d766a6fee1a50d18.jpg"
 pm_caption = "`E.D.I.T.H.` **MODE ON**"
 #@command(outgoing=True, pattern="^.on$")
-@borg.on(admin_cmd(pattern=r"alive"))
+@borg.on(admin_cmd(pattern=r"on"))
 async def amireallyalive(on):
     chat = await alive.get_chat()
     await mode.delete()
-    """ For .alive command, check if the bot is running.  """
+    """ For .on command, check if the bot is running.  """
     await borg.send_file(mode.chat_id, PM_IMG,caption=pm_caption)
     await mode.delete() 
