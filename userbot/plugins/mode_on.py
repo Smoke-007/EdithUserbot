f30c938149d77a44b2d2712a5c3c1927ee5674a2
TeamAtul <68645655+TeamAtul@users.noreply.github.com>
2020-09-16 20:49:44 +0530

    Update mode_on.py

--- userbot/plugins/mode_on.py
+++ userbot/plugins/mode_on.py
@@ -1,7 +1,3 @@
-import asyncio
-from telethon import events
-from uniborg.util import admin_cmd
-from userbot import ALIVE_NAME
 PM_IMG = "https://telegra.ph/file/e5c05d766a6fee1a50d18.jpg"
 pm_caption = "`E.D.I.T.H.` **MODE ON**"
 #@command(outgoing=True, pattern="^.mode$")
